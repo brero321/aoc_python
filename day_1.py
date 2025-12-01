bd0ed9cda4151b243666381cb74d226e048162e6
Andrew Brereton <andrewjbrereton31@gmail.com>
2025-12-01 22:36:13 +0000

    tidy code

--- day_1.py
+++ day_1.py
@@ -6,19 +6,10 @@ with open("day_1_input.txt") as f:
 def count_pointer_at_zero(input_list):
     pos = 50
     counter = 0
+    input_list = [(-1 if x[0] == 'L' else 1) * int(x[1:]) for x in input_list]
     for x in input_list:
-        if x[0] == 'L':
-            pos = pos - int(x[1:])
-            if pos == 100:
-                pos = 0
-            if pos < 0:
-                pos = pos % 100
-        if x[0] == 'R':
-            pos = pos + int(x[1:])
-            if pos == 100:
-                pos = 0
-            if pos > 100:
-                pos = pos % 100
+        pos = pos + x
+        pos = pos % 100
         if pos == 0:
             counter = counter + 1
     return(counter)
@@ -27,6 +18,7 @@ def count_zero_crossings(input_list):
     pos = 50
     counter = 0
     zero_count = 0
+    input_list = [(-1 if x[0] == 'L' else 1) * int(x[1:]) for x in input_list]
     for x in input_list:
         incr = int(x[1:])
         if x[0] == 'L':
@@ -53,3 +45,26 @@ def count_zero_crossings(input_list):
 
 count_pointer_at_zero(input_list)
 count_zero_crossings(input_list)
+
+
+def count_zero_crossings(input_list):      
+    pos = 50
+    counter = 0
+    zero_count = 0
+    input_list = [(-1 if x[0] == 'L' else 1) * int(x[1:]) for x in input_list]
+    for x in input_list:
+        total_diff = pos + x
+        if total_diff <= 0:
+            div_by_100 = abs(math.ceil(total_diff / 100))
+            if pos == 0:
+                zero_count = div_by_100
+            else:
+                zero_count = div_by_100 + 1
+        elif total_diff >= 100:
+                div_by_100 = math.floor(total_diff/100)
+                zero_count = div_by_100
+        else:
+            zero_count = 0
+        pos = (pos + x) % 100
+        counter = counter + zero_count
+    return(counter)
