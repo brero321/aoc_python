d55b9675e2e3c44d8dd81b4a605feb1e4946de9f
Andrew Brereton <andrewjbrereton31@gmail.com>
2025-12-01 22:37:18 +0000

    remove prev function version

--- day_1.py
+++ day_1.py
@@ -20,39 +20,6 @@ def count_zero_crossings(input_list):
     zero_count = 0
     input_list = [(-1 if x[0] == 'L' else 1) * int(x[1:]) for x in input_list]
     for x in input_list:
-        incr = int(x[1:])
-        if x[0] == 'L':
-            total_diff = pos - incr
-            if total_diff <= 0:
-                div_by_100 = abs(math.ceil(total_diff / 100))
-                if pos == 0:
-                    zero_count = div_by_100
-                else:
-                    zero_count = div_by_100 + 1
-            else:
-                zero_count = 0
-            pos = (pos - incr) % 100
-        if x[0] == 'R':
-            total_diff = pos + incr
-            if total_diff >= 100:
-                div_by_100 = math.floor(total_diff/100)
-                zero_count = div_by_100
-            else:
-                zero_count = 0
-            pos = (pos + incr) % 100
-        counter = counter + zero_count
-    return(counter)
-
-count_pointer_at_zero(input_list)
-count_zero_crossings(input_list)
-
-
-def count_zero_crossings(input_list):      
-    pos = 50
-    counter = 0
-    zero_count = 0
-    input_list = [(-1 if x[0] == 'L' else 1) * int(x[1:]) for x in input_list]
-    for x in input_list:
         total_diff = pos + x
         if total_diff <= 0:
             div_by_100 = abs(math.ceil(total_diff / 100))
